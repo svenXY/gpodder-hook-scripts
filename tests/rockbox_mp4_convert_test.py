5648602193812d11c23e722d711c173e9e919ba2
Bernd Schlapsi <brot@gmx.info>
2011-11-10 07:16:42 +0100

    refactored rockbox-hook - moved all logic into the hook class and also change behaviour of notification a little bit.

--- tests/rockbox_mp4_convert_test.py
+++ tests/rockbox_mp4_convert_test.py
@@ -18,31 +18,33 @@ class TestRockboxMP4Convert(unittest.TestCase):
 
         self.episode = self.podcast.get_episodes()[episode_no]
         self.filename = self.episode._episode.local_filename(create=False, check_only=True)
+        
+        self.rb_hook = hook.gPodderHooks() 
 
     def tearDown(self):
         self.client._db.close()
 
-        converted_file = hook.get_rockbox_filename(self.filename)
+        converted_file = self.rb_hook._get_rockbox_filename(self.filename)
         if (os.path.exists(converted_file)):
             os.remove(converted_file)
 
     def test_file_renaming(self):
         self.assertIsNotNone(self.filename)
         self.assertEqual(os.path.basename(self.filename), 'MattCutts_2011U.mp4')
-        self.assertEqual(hook.get_rockbox_filename(self.filename),
+        self.assertEqual(self.rb_hook._get_rockbox_filename(self.filename),
             os.path.join(os.path.dirname(self.filename), 'MattCutts_2011U.mpg'))
-
+        
     def test_calc_resolution(self):
-        resolution = hook.calc_resolution(512, 288, 224.0, 176.0)
+        resolution = self.rb_hook._calc_resolution(512, 288, 224.0, 176.0)
         self.assertEqual(resolution, (224, 126))
 
-        resolution = hook.calc_resolution(1024, 768, 224.0, 176.0)
+        resolution = self.rb_hook._calc_resolution(1024, 768, 224.0, 176.0)
         self.assertEqual(resolution, (224, 168))
 
     def test_mp4convert(self):
         self.assertIsNotNone(self.filename)
         self.assertEqual(self.episode._episode.title, 'TED: Matt Cutts: Try something new for 30 days - Matt Cutts (2011)')
 
-        new_filename = hook.convert_mp4(self.filename, hook.DEFAULT_PARAMS)
+        new_filename = self.rb_hook._convert_mp4(self.filename, hook.DEFAULT_PARAMS)
         self.assertIsNotNone(new_filename)
         self.assertTrue(os.path.exists(new_filename))
