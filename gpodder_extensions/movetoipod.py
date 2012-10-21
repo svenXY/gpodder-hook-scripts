afa5397a7d00a7ec449747ca790809cb015b8037
Paul Ortyl <ortylp@3miasto.net.pl>
2012-10-21 22:10:02 +0200

    movetoipod: fix gui update, support conversion from .ogg files

--- gpodder_extensions/movetoipod.py
+++ gpodder_extensions/movetoipod.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 # Send files to iPod using libgpod
-# Copyright (c) 2012-10-15 Paul Ortyl <ortylp@3miasto.net.pl>
+# Copyright (c) 2012-10-21 Paul Ortyl <ortylp@3miasto.net.pl>
 # Licensed under the same terms as gPodder itself
 '''
 Extention to gPodder for sending (moving) files to iPod from context menu
 '''
 
 # missing features:
-# * refresh of UI/model after episode gets deleted
 # * user feedback
-# * automatic conversion to file format supported by iPod (assume mp3)
-
 
 import os
 import gpodder
 import logging
+import subprocess
 logger = logging.getLogger(__name__)
 
 _are_libraries_available = True
 try:
     import gpod
-    import eyeD3
+    from mutagen.oggvorbis import OggVorbis
+    from mutagen.mp3       import MP3
 except Exception, e:
     _are_libraries_available = False
 
@@ -29,21 +28,63 @@ _ = gpodder.gettext
 __title__ = _('Move file(s) to iPod from context menu')
 __description__ = _('Move file(s) to iPod from context menu')
 __authors__ = 'Paul Ortyl <ortylp@3miasto.net.pl>'
+__only_for__ = 'gtk'
 
 DefaultConfig = {
     'context_menu': True,  # Show item in context menu
 }
+
+
+def _convert_to_mp3(filename):
+    '''Convert file to temporary mp3
+        @param filename: filename of the audio file to be converted
+        @return: file name of the temporary file in mp3 format, this file should be deleted afterwards
+    '''
+    fnameMP3 = filename + '.mp3'
+    # remove file so that avconv wont get confused
+    if (os.path.exists(fnameMP3)): os.unlink(fnameMP3)
+    cmd = ['avconv', '-i', filename, fnameMP3]
+    avconv = subprocess.Popen(cmd)
+    avconv.communicate()
+
+    if 0 == avconv.returncode:
+        return fnameMP3
+    else:
+        os.unlink(fnameMP3)
+        return None
+
+def _get_MP3_tags(filename):
+    tagsMP3 = MP3(filename)
+    tags = {}
+    tags['album'] = tagsMP3['TALB']
+    tags['title'] = tagsMP3['TIT2']
+    tags['artist'] = tagsMP3['TPE1']
+    tags['length'] = tagsMP3.info.length * 1000
+    tags['genre'] = tagsMP3['TCON']
+    return tags
+
+def _get_OGG_tags(filename):
+    tagsOGG = OggVorbis(filename)
+    tags = {}
+    tags['album'] = tagsOGG['album']
+    tags['title'] = tagsOGG['title']
+    tags['artist'] = tagsOGG['artist']
+    tags['length'] = tagsOGG.info.length * 1000
+    tags['genre'] = 'Podcast'
+    return tags
 
 class gPodderExtension:
     def __init__(self, container):
 
         self.container = container
         self.config = self.container.config
+        self.ui = None
         # use default evironment variable as defined for gtkpod
         self.ipod_mount = os.getenv('IPOD_MOUNTPOINT')
 
-    def on_episode_downloaded(self, episode):
-        True
+    def on_ui_object_available(self, name, ui_object):
+        '''Get reference onto UI object, needed for triggering UI update after move'''
+        if name == 'gpodder-gtk': self.ui = ui_object
 
     def on_episodes_context_menu(self, episodes):
         if not _are_libraries_available:
@@ -52,8 +93,12 @@ class gPodderExtension:
         if not self.config.context_menu:
             return None
 
-        if 'audio/mpeg' not in [e.mime_type for e in episodes
-            if e.mime_type is not None and e.file_exists()]:
+        mpeg = 'audio/mpeg' in [e.mime_type for e in episodes
+            if e.mime_type is not None and e.file_exists()]
+        ogg = 'audio/ogg'  in [e.mime_type for e in episodes
+            if e.mime_type is not None and e.file_exists()]
+
+        if not mpeg and not ogg:
             return None
 
         if self._find_ipod():
@@ -78,48 +123,56 @@ class gPodderExtension:
             if episode.file_type() != 'audio':
                 return
 
-            if extension.lower() != '.mp3':
+            sent = False  # set to true if file transfer was successful
+            if extension.lower() == '.mp3':
+                sent = self.send_file_to_ipod(itdb, filename, _get_MP3_tags(filename))
+            elif extension.lower() == '.ogg':
+                tmpname = _convert_to_mp3(filename)
+                if tmpname:
+                    sent = self.send_file_to_ipod(itdb, tmpname, _get_OGG_tags(filename))
+                    os.unlink(tmpname);
+            else:
                 return
 
-            if self.send_file_to_ipod(itdb, filename):
+            if sent:
                 episode.mark_old()
-                episode.delete_from_disk()
-                logger.info("File '%s' has been deleted from gPoddes database and filesystem" % filename)
+                logger.info("File '%s' has been marked 'old'" % filename)
+                if not episode.archive:
+                  episode.delete_from_disk()
+                  logger.info("File '%s' has been deleted from gPodder database and file system"
+                              % filename)
+                # update UI
+                if self.ui:
+                  self.ui.episode_list_status_changed([episode])
 
         gpod.itdb_free(itdb)
 
-    def send_file_to_ipod(self, itdb, fname):
-        if eyeD3.isMp3File(fname):
-            logger.debug("Copying file '%s' to iPod..." % fname)
-            podcasts = gpod.itdb_playlist_podcasts(itdb)
-            af = eyeD3.Mp3AudioFile(fname, eyeD3.ID3_ANY_VERSION)
-            tag = af.getTag()
-            track = gpod.itdb_track_new()
-            track.visible = 1
-            track.filetype = "mp3"
-            track.ipod_path = fname
-            track.tracklen = af.getPlayTime() * 1000
-            track.album = str(tag.getAlbum())
-            track.artist = str(tag.getArtist())
-            track.title = str(tag.getTitle())
-            track.genre = str(tag.getGenre())
-            track.playcount = 0
-            gpod.itdb_track_add(itdb, track, -1)
-            gpod.itdb_playlist_add_track(podcasts, track, -1)
-            is_copied = gpod.itdb_cp_track_to_ipod(track, fname, None)
-            if is_copied:
-                logger.info("File '%s' has been successfully copied to iPod" % fname)
-            else:
-                # roll back
-                logger.error("File '%s' could not be copied to iPod" % fname)
-                gpod.itdb_playlist_remove_track(podcasts, track)
-                gpod.itdb_track_remove(track)
-                track = None
-            gpod.itdb_write(itdb, None)
-            return is_copied
+    def send_file_to_ipod(self, itdb, fname, tags):
+        logger.debug("Copying file '%s' to iPod..." % fname)
+        podcasts = gpod.itdb_playlist_podcasts(itdb)
+        track = gpod.itdb_track_new()
+        track.visible = 1
+        track.filetype = "mp3"
+        track.ipod_path = fname
+        track.album = str(tags['album'])
+        track.artist = str(tags['artist'])
+        track.title = str(tags['title'])
+        track.genre = str(tags['genre'])
+        track.tracklen = tags['length']
+        track.playcount = 0
+        gpod.itdb_track_add(itdb, track, -1)
+        gpod.itdb_playlist_add_track(podcasts, track, -1)
+        is_copied = gpod.itdb_cp_track_to_ipod(track, fname, None)
+        if is_copied:
+            logger.info("File '%s' has been successfully copied to iPod" % fname)
         else:
-            logger.error("File format for %s is not mp3, skipping" % fname)
-            return False
+            # roll back
+            logger.error("File '%s' could not be copied to iPod" % fname)
+            gpod.itdb_playlist_remove_track(podcasts, track)
+            gpod.itdb_track_remove(track)
+        track = None
+        gpod.itdb_write(itdb, None)
+        return is_copied
 
     def _find_ipod(self):
         '''Try to autodetect mount point of ipod and set the internal path to ipod'''
